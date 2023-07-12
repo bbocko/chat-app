ba1842bbc918dd5000a7be7da752e1a17e4a6a17
Blaž <blaz.bocko1@gmail.com>
2023-07-12 23:23:59 +0200

    Add basic use cases for chat app

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { dirname, join } from "path";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import knex from "knex";
+import moment from "moment";
 
 const connection = {
     client: "pg",
@@ -11,8 +12,8 @@ const connection = {
         host: "localhost",
         user: "blaz",
         password: "chatapp123",
-        database: "chat-app-db"
-    }
+        database: "chat-app-db",
+    },
 };
 
 const db = knex(connection);
@@ -31,53 +32,135 @@ const io = new Server(httpServer);
 app.use(express.json());
 
 // API to get all chat rooms
-app.get('/api/rooms', async (req, res) => {
+app.get("/api/rooms", async (req, res) => {
     try {
-        const rooms = await db('chat_rooms').select('id', 'name');
+        const rooms = await db("chat_rooms").select("id", "name");
         res.json(rooms);
     } catch (error) {
-        console.error('Error getting chat rooms:', error);
-        res.status(500).json({ message: 'Failed to get chat rooms' });
+        console.error("Error getting chat rooms:", error);
+        res.status(500).json({ message: "Failed to get chat rooms" });
     }
 });
 
 // API for creating new room
-app.post('/api/rooms', async (req, res) => {
+app.post("/api/rooms", async (req, res) => {
     const { roomName } = req.body;
 
     // Validate the roomName field
     if (!roomName) {
-        return res.status(400).json({ message: 'Room name is required' });
+        return res.status(400).json({ message: "Room name is required" });
     }
 
     try {
-        const newRoom = await db('chat_rooms')
+        const newRoom = await db("chat_rooms")
             .insert({ name: roomName })
-            .returning('*');
-
-        // emit the new room to connected clients
-        io.emit('newRoom', newRoom[0]);
+            .returning("*");
 
         res.status(201).json(newRoom);
-
     } catch (error) {
-        console.error('Error creating new room:', error);
-        res.status(500).json({ message: 'Failed to create new room' });
+        console.error("Error creating new room:", error);
+        res.status(500).json({ message: "Failed to create new room" });
     }
 });
 
+// API for joining a room
+app.post("/api/join", async (req, res) => {
+    const { roomId } = req.body;
+
+    try {
+        // check if the roomId exists in the database
+        const room = await db("chat_rooms").where("id", roomId).first();
+        if (!room) {
+            return res.status(404).json({ message: "Room not found" });
+        }
+
+        // send a success response
+        res.status(200).json({ message: "Joined room successfully" });
+    } catch (error) {
+        console.error("Error joining room:", error);
+        res.status(500).json({ message: "Failed to join room" });
+    }
+});
+
+function formatMessage(username, text) {
+    return {
+        username,
+        text,
+        time: moment().format("hh:mm a"),
+    };
+}
+
+const botName = "Chat Bot";
+
+// Array to store users in a room
+const users = [];
+
+// Fetch the room name from the database and emit roomUsers
+async function emitRoomUsers(roomId) {
+    try {
+        const roomData = await db("chat_rooms").where("id", roomId).first();
+        if (!roomData) {
+            throw new Error("Room not found");
+        }
+
+        const { name } = roomData;
+
+        // Emit the room name and users to the client
+        io.to(roomId).emit("roomUsers", {
+            room: name,
+            users: users.filter((u) => u.room === roomId),
+        });
+    } catch (error) {
+        console.error("Error getting room data:", error);
+    }
+}
+
 // run when user connects
 io.on("connection", (socket) => {
-    //send greetings message to user
-    socket.emit("message", "Welcome to #Room 1");
-    //broadcast to other when user connects
-    socket.broadcast.emit("message", "A user has joined the chat");
+    socket.on("joinRoom", async ({ room, username }) => {
+        const user = { id: socket.id, room, username };
 
-    //broadcast to other when user disconnects
+        socket.join(user.room);
+        users.push(user);
+
+        socket.emit("message", formatMessage(botName, "Welcome to chat"));
+
+        // Fetch the room name from the database and emit roomUsers
+        emitRoomUsers(room);
+
+        socket.to(user.room).emit(
+            "message",
+            formatMessage(botName, `${user.username} has joined the chat`)
+        );
+    });
+
+    // listen to chatMessage
+    socket.on("chatMessage", (msg) => {
+        const user = users.find((u) => u.id === socket.id);
+        if (user) {
+            io.to(user.room).emit("message", formatMessage(user.username, msg));
+        }
+    });
+
+    // run when user disconnects
     socket.on("disconnect", () => {
-        socket.broadcast.emit("message", "A user has left the chat");
-    })
+        const userIndex = users.findIndex((u) => u.id === socket.id);
+        if (userIndex !== -1) {
+            const user = users[userIndex];
+            users.splice(userIndex, 1);
+
+            io.to(user.room).emit(
+                "message",
+                formatMessage(botName, `${user.username} has left the chat`)
+            );
+
+            // Emit the room name and users to the client
+            emitRoomUsers(user.room);
+        }
+    });
 });
 
 const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+httpServer.listen(PORT, () =>
+    console.log(`Server is running on port ${PORT}`)
+);
